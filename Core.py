3130091a7b9e350fedbbd3cec38133bf2321dc93
rahmat nazali s <rahmatnazali95@gmail.com>
2018-04-10 10:00:20 +0700

    proposing updated code to handle multiple file pointer

--- Core.py
+++ Core.py
@@ -1,36 +1,44 @@
 import PyPDF2
-import datetime, time
+import datetime, time, os
 
 class PDFMerger():
 
     def __init__(self):
         self.pdfWriter = PyPDF2.PdfFileWriter()
-        self.pdfInputPointer = None
+        self.pdfInputPointers = []
 
     def generateFileName(self):
         timestamp = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H_%M_%S')
         return 'result/combined_' + timestamp + '.pdf'
 
     def mergePDF(self, fileList):
+        # todo: add exception if file not exist
+
+        # open all PDF files
         for file in fileList:
-            self.pdfInputPointer = open(file, 'rb')
-            print('merging', file, ': ', self.pdfInputPointer.__sizeof__(), 'kb')
-            pdfReader = PyPDF2.PdfFileReader(self.pdfInputPointer)
+            self.pdfInputPointers.append(open(file, 'rb'))
+            print('\tmerging', file, ': ', os.stat(file).st_size/1000, 'kb')
+
+        # read all the data
+        for filePointer in self.pdfInputPointers:
+            pdfReader = PyPDF2.PdfFileReader(filePointer)
 
             for pageNumber in range(pdfReader.numPages):
                 pageObj = pdfReader.getPage(pageNumber)
                 self.pdfWriter.addPage(pageObj)
 
-        pdfOutputFile = open(self.generateFileName(), 'wb')
-        print('final file:', pdfOutputFile.__sizeof__())
-        self.pdfWriter.write(pdfOutputFile)
-        print('final file:', pdfOutputFile.__sizeof__())
-        pdfOutputFile.close()
-        self.pdfInputPointer.close()
+        # write to a new PDF File
+        outputFileName = self.generateFileName()
+        with open(outputFileName, 'wb') as pdfOutputFile:
+            self.pdfWriter.write(pdfOutputFile)
+            print('exported', outputFileName, ':', os.stat(outputFileName).st_size/1000, 'kb')
 
+        # close all file pointer
+        for filePointer in self.pdfInputPointers:
+            filePointer.close()
 
 if __name__ == '__main__':
-    print('main')
+    print('Python PDF Merger')
     pdfMergerInstance = PDFMerger()
     pdfMergerInstance.mergePDF([
         'pdf-sample_1.pdf',
